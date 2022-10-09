e1114dd4aea639ad0166858c6cf0024278917262
Aneesh Tigga <aneeshtigga@gmail.com>
2022-10-09 22:42:34 +0530

    add the checksum code

---
+++ checksum-gen.py
@@ -0,0 +1,14 @@
+# author: Aneesh Tigga (aneeshtigga@gmail.com)
+
+import sys
+from tabnanny import check
+
+with open(sys.argv[1], 'rb+') as f:
+    ram = bytearray(f.read())
+    checksum = 0xff
+    for c in ram[0x2598:0x3523]:
+        checksum -= c
+        ram[0x3523] = checksum&0xff
+        f.seek(0,0)
+        f.write(ram)
+        print("checksum {} written".format(checksum&0xff))
